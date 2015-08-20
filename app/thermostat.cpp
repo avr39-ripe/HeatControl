c2f255c4500f18dc2bd1e44fafe6eb8db9505e3a
Alexander V. Ribchansky <avr39ripe@gmail.com>
2015-08-20 18:13:17 +0300

    More implemented, still not working

--- app/thermostat.cpp
+++ app/thermostat.cpp
@@ -26,6 +26,69 @@ void HeatingSystem::check_room(uint8_t room_id)
 		;
 	}
 }
+
+void HeatingSystem::room_turn_on(uint8_t room_id)
+{
+	if (_mode == WARMY && _rooms[room_id]->low_t_control_pin != NO_PIN) //room with lo_temp hot floor and MAY BE hi_temp heaters
+	{
+		setState(out_reg,_rooms[room_id]->low_t_control_pin, true);
+		_pumps[LOW_T_PUMP]->turn_on();
+		caldron_turn_on();
+	}
+	else if (_mode == WARMY && _rooms[room_id]->hi_t_control_pin != NO_PIN) //room with JUST hi_temp heater
+	{
+		setState(out_reg,_rooms[room_id]->hi_t_control_pin, true);
+		_pumps[HI_T_PUMP]->turn_on();
+		caldron_turn_on();
+	}
+
+	else if (_mode == COLDY)
+	{
+		if ( _rooms[room_id]->low_t_control_pin != NO_PIN)
+		{
+			setState(out_reg, _rooms[room_id]->low_t_control_pin, true);
+			_pumps[LOW_T_PUMP]->turn_on();
+			caldron_turn_on();
+		}
+		if (_rooms[room_id]->hi_t_control_pin != NO_PIN)
+		{
+			setState(out_reg, _rooms[room_id]->hi_t_control_pin, true);
+			_pumps[HI_T_PUMP]->turn_on();
+			caldron_turn_on();
+		}
+	}
+}
+
+void HeatingSystem::room_turn_off(uint8_t room_id)
+{
+	if (_mode == WARMY && _rooms[room_id]->low_t_control_pin != NO_PIN) //room with lo_temp hot floor and MAY BE hi_temp heaters
+	{
+		setState(out_reg,_rooms[room_id]->low_t_control_pin, false);
+		_pumps[LOW_T_PUMP]->turn_off();
+		caldron_turn_off();
+	}
+	else if (_mode == WARMY && _rooms[room_id]->hi_t_control_pin != NO_PIN) //room with JUST hi_temp heater
+	{
+		setState(out_reg,_rooms[room_id]->low_t_control_pin, false);
+		_pumps[HI_T_PUMP]->turn_off();
+		caldron_turn_off();
+	}
+
+	else if (_mode == COLDY)
+	{
+		if ( _rooms[room_id]->low_t_control_pin != NO_PIN)
+		{
+			this->_roomTimer.initializeMs(_room_coldy_lo_t_off_delay * 1000, myTimerDelegate(&HeatingSystem::_room_coldy_lo_t_off_delayed, this, room_id)).start(false);
+		}
+		if (_rooms[room_id]->hi_t_control_pin != NO_PIN)
+		{
+			setState(out_reg, _rooms[room_id]->hi_t_control_pin, false);
+			_pumps[HI_T_PUMP]->turn_off();
+			caldron_turn_off();
+		}
+	}
+}
+
 //Pump implementation
 
 Pump::Pump(uint8_t pump_pin, uint8_t pump_on_delay, uint8_t pump_off_delay)
@@ -39,11 +102,9 @@ void Pump::turn_on()
 {
 	if (_consumers == 0)
 	{
-		_consumers++;
-		this->pumpTimer.initializeMs(_pump_on_delay * 1000, TimerDelegate(&Pump::turn_on_delayed, this)).start(false);
+		this->_pumpTimer.initializeMs(_pump_on_delay * 1000, TimerDelegate(&Pump::turn_on_delayed, this)).start(false);
 	}
-	else
-		_consumers++;
+	_consumers++;
 
 
 }
@@ -53,7 +114,7 @@ void Pump::turn_off()
 	_consumers--;
 	if (_consumers == 0)
 	{
-		this->pumpTimer.initializeMs(_pump_off_delay * 1000, TimerDelegate(&Pump::turn_off_delayed, this)).start(false);
+		this->_pumpTimer.initializeMs(_pump_off_delay * 1000, TimerDelegate(&Pump::turn_off_delayed, this)).start(false);
 	}
 
 }
@@ -70,10 +131,56 @@ void Pump::turn_off_delayed()
 
 //HeatingSystem implementation
 
-HeatingSystem::HeatingSystem(uint8_t mode_pin, uint8_t caldron_pin, uint8_t caldron_on_delay)
+HeatingSystem::HeatingSystem(uint8_t mode_pin, uint8_t caldron_pin, uint8_t caldron_on_delay, uint8_t _room_coldy_off_delay)
 {
 	this->_mode_pin = mode_pin;
 	this->_caldron_pin = caldron_pin;
 	this->_caldron_on_delay = caldron_on_delay;
 	this->_mode = WARMY;
+}
+
+void HeatingSystem::check_mode()
+{
+	int mode_state = pinState(_mode_pin);
+
+		if ( mode_state & JUSTPRESSED)
+		{
+			_mode == COLDY; //Thermostat turns ON when there is too cold outside
+		}
+
+		if ( mode_state & JUSTRELEASED)
+		{
+			_mode == WARMY; //Thermostat turns OFF when there is too hot outside
+		}
+}
+
+void HeatingSystem::caldron_turn_on()
+{
+	if (_caldron_consumers == 0)
+		{
+			this->_caldronTimer.initializeMs(_caldron_on_delay * 1000, TimerDelegate(&HeatingSystem::_caldron_turn_on_delayed, this)).start(false);
+		}
+		_caldron_consumers++;
+}
+
+void HeatingSystem::caldron_turn_off()
+{
+	_caldron_consumers--;
+	if (_caldron_consumers == 0)
+	{
+		setState(out_reg, _caldron_pin, false);
+	}
+
+}
+
+void HeatingSystem::_caldron_turn_on_delayed()
+{
+	setState(out_reg, _caldron_pin, true);
+}
+
+void HeatingSystem::_room_coldy_lo_t_off_delayed(uint8_t room_id)
+{
+	setState(out_reg, _rooms[room_id]->low_t_control_pin, false);
+	_pumps[LOW_T_PUMP]->turn_off();
+	caldron_turn_off();
 }
--- include/thermostat.h
+++ include/thermostat.h
@@ -11,6 +11,8 @@
 //When there is no pin use this
 #define NO_PIN 255
 
+typedef Delegate<void(uint8_t)> myTimerDelegate;
+
 struct Room
 {
 	uint8_t thermostat_pin;
@@ -24,36 +26,41 @@ public:
 	Pump(uint8_t pump_pin, uint8_t pump_on_delay, uint8_t pump_off_delay);
 	void turn_on();
 	void turn_off();
+	uint8_t _pump_on_delay;
+	uint8_t _pump_off_delay;
 private:
 	void turn_on_delayed();
 	void turn_off_delayed();
 	uint8_t _pump_pin;
-	uint8_t _pump_on_delay;
-	uint8_t _pump_off_delay;
 	uint8_t _consumers = 0;
-	Timer pumpTimer;
+	Timer _pumpTimer;
 
 };
 
 class HeatingSystem
 {
 public:
-	HeatingSystem(uint8_t mode_pin, uint8_t caldron_pin, uint8_t caldron_on_delay);
-	void turn_on_caldron();
-	void turn_off_caldron();
+	HeatingSystem(uint8_t mode_pin, uint8_t caldron_pin, uint8_t caldron_on_delay, uint8_t _room_coldy_off_delay);
+	void caldron_turn_on();
+	void caldron_turn_off();
 //	void turn_on_pump(uint8_t pump_id);
 //	void turn_off_pump(uint8_t pump_id);
 	void check_room(uint8_t room_id);
-	void turn_on_room(uint8_t room_id);
-	void turn_off_room(uint8_t room_id);
-
+	void room_turn_on(uint8_t room_id);
+	void room_turn_off(uint8_t room_id);
+	void check_mode();
+	void check(); //run periodicaly by timer to check mode and rooms
+	uint8_t _caldron_on_delay;
+	uint8_t _room_coldy_lo_t_off_delay;
 private:
+	void _caldron_turn_on_delayed();
+	void _room_coldy_lo_t_off_delayed(uint8_t room_id);
 	uint8_t _mode;
 	uint8_t _caldron_pin;
-	uint8_t _caldron_on_delay;
 	uint8_t _caldron_consumers = 0;
 	uint8_t _mode_pin;
-
+	Timer _caldronTimer;
+	Timer _roomTimer;
 	Room* _rooms[9];
 	Pump* _pumps[2];
 };
